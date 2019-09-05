7ae1ee1c70d20270ab379b0d1262cc940457287c
Paul Chen <32553156+paulmchen@users.noreply.github.com>
2019-09-05 14:45:45 -0400

    Update employee.sql

--- sql/employee.sql
+++ sql/employee.sql
@@ -6,3 +6,7 @@ CREATE TABLE mypolls.employee (
 	lastname VARCHAR(64) NOT NULL,
     gender integer NOT NULL
 );
+
+insert into mypolls.employee values (1, "Smith", "John", 0);
+insert into mypolls.employee values (2, "Jane", "Swandar", 1);
+insert into mypolls.employee values (3, "Bob", "Staney", 0);
