e2b5a7cb7560e972d4e76902697a5c274bf8b52d
Paul Chen <32553156+paulmchen@users.noreply.github.com>
2019-09-05 16:14:35 -0400

    Update loadconfig.py

--- loadconfig.py
+++ loadconfig.py
@@ -20,32 +20,32 @@ class Config:
         )
 
         # load db settings from environment variables if set
-        if os.environ.get('relational_database.parameters.db_host') is not None:
-            self.db_config['host'] = os.environ.get('relational_database.parameters.db_host')
+        if os.environ.get('relational_database_parameters_db_host') is not None:
+            self.db_config['host'] = os.environ.get('relational_database_parameters_db_host')
 
-        if os.environ.get('relational_database.parameters.db_user') is not None:
-            self.db_config['user'] = os.environ.get('relational_database.parameters.db_user')
+        if os.environ.get('relational_database_parameters_db_user') is not None:
+            self.db_config['user'] = os.environ.get('relational_database_parameters_db_user')
 
-        if os.environ.get('relational_database.parameters.db_port') is not None:
-            self.db_config['port'] = int(os.environ.get('relational_database.parameters.db_port'))
+        if os.environ.get('relational_database_parameters_db_port') is not None:
+            self.db_config['port'] = int(os.environ.get('relational_database_parameters_db_port'))
 
-        if os.environ.get('relational_database.parameters.password') is not None:
-            self.db_config['password'] = os.environ.get('relational_database.parameters.password')
+        if os.environ.get('relational_database_parameters_password') is not None:
+            self.db_config['password'] = os.environ.get('relational_database_parameters_password')
 
-        if os.environ.get('relational_database.parameters.db_name') is not None:
-            self.db_config['db'] = os.environ.get('relational_database.parameters.db_name')
+        if os.environ.get('relational_database_parameters_db_name') is not None:
+            self.db_config['db'] = os.environ.get('relational_database_parameters_db_name')
 
         print(self.db_config)
 
         # load redis settings from environment variables if set
-        if os.environ.get('distributed_session.parameters.host') is not None:
-            self.cache_config['host'] = os.environ.get('distributed_session.parameters.host')
+        if os.environ.get('distributed_session_parameters_host') is not None:
+            self.cache_config['host'] = os.environ.get('distributed_session_parameters_host')
 
-        if os.environ.get('distributed_session.parameters.port') is not None:
-            self.cache_config['port'] = os.environ.get('distributed_session.parameters.port')
+        if os.environ.get('distributed_session_parameters_port') is not None:
+            self.cache_config['port'] = os.environ.get('distributed_session_parameters_port')
 
-        if os.environ.get('distributed_session.parameters.password') is not None:
-            self.cache_config['password'] = int(os.environ.get('distributed_session.parameters.password'))
+        if os.environ.get('distributed_session_parameters_password') is not None:
+            self.cache_config['password'] = int(os.environ.get('distributed_session_parameters_password'))
 
         print(self.cache_config)
 
